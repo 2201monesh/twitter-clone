bdaeb96b70ff7355e3ecf37dbef4c133e55dc15a
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-07-21 18:15:33 +0530

    Styling widgets

--- src/Widgets.js
+++ src/Widgets.js
@@ -9,6 +9,23 @@ function Widgets() {
         <SearchIcon className='widgets--searchIcon' />
         <input placeholder='Search Twitter' type="text" />
       </div>
+
+      <div className="widgets--widgetContainer">
+        <h2>What's happening</h2>
+
+        <TwitterTweetEmbed tweetId={"858551177860055040"} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="GoyalMonesh"
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={""}
+          options={{ text: "#This message is from a twitter clone made by Monesh Goyal", via: "GoyalMonesh" }}
+        />  
+      </div>
     </div>
   )
 }
